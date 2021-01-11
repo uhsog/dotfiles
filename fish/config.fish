83269e58b25c12644d70820f619d74edbbc3e63f
uhsog <30695524+uhsog@users.noreply.github.com>
2021-01-11 18:26:39 +0900

    Fix config for fish
    - To show more git information

--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,21 @@
 # Alias
 alias cl='clear'
+alias cdw='cd $HOME/workspace'
+alias gg='git grep'
 
 # ENV
 set -U FZF_LEGACY_KEYBINDINGS 0
 
+# PATH
+# set -x PATH /target/path $PATH
+
 # Fish git prompt
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_upstream_ahead green
-set __fish_git_prompt_color_upstream_behind red
+set __fish_git_prompt_show_informative_status 'yes'
 
 # Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
-set __fish_git_prompt_char_stagedstate '>> '
+set __fish_git_prompt_char_cleanstate '✔'
+set __fish_git_prompt_char_dirtystate '∆'
+set __fish_git_prompt_char_stagedstate '≫'
 set __fish_git_prompt_char_untrackedfiles '☡'
 set __fish_git_prompt_char_stashstate '↩'
 set __fish_git_prompt_char_upstream_ahead '+'
